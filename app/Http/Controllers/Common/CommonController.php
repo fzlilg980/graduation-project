387416efec30c369e0efdb262d98c3ddf1043f42
fzlilg980 <wuduanmang1972@126.com>
2021-06-13 15:08:40 +0800

    返回视图简单化

--- app/Http/Controllers/Common/CommonController.php
+++ app/Http/Controllers/Common/CommonController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers\Common;
 
 use App\Http\Controllers\Controller;
 
-class CommonController extends Controller{
-    
-    public function fetch()
+class CommonController extends Controller
+{
+    public function view($data = [], $view = null)
     {
+        if ($view) {
+            $view = view($view);
+        } else {
+            $action = \Route::currentRouteAction();
+            $actions = explode('\\', $action);
+            $len = count($actions);
+            $namespace = $actions[$len - 2];
+            $_controller = $actions[$len - 1];
+            $_controller = explode('@', $_controller);
+            $controller = $_controller[0];
+            $method = $_controller[1];
+            $namespace = strtolower($namespace);
+            $controller = strtolower(substr($controller, 0, -10));
+            $view = view($namespace.'.'.$controller.'.'.$method);
+        }
+        
+        foreach ($data as $key => $val) {
+            $view = $view->with($key, $val);
+        }
 
+        return $view;
     }
 }
--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -14,8 +14,20 @@ class IndexController extends Index
 
     public function login()
     {
-        $info = User::all();
-        return $info;
+        $route = \Route::current();
+
+        $name = \Route::currentRouteName();
+        
+        $action = \Route::currentRouteAction();
+
+        // var_dump($route);
+        echo '<br>';
+        // echo $name;
+        echo '<br>';
+        echo $action;
+        $data = [];
+        $data['action'] = $action;
+        return $this->view($data);
     }
 
 }
